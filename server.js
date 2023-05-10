7258e80b721b5c14e397f1a5d14c2946106ef541
whythochat <41367501+whythochat@users.noreply.github.com>
2023-05-10 12:30:00 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,54 @@
+/**
+ * This is the main Node.js server script for your project
+ * Check out the two endpoints this back-end API provides in fastify.get and fastify.post below
+ */
+
+const path = require("path");
+
+// Require the fastify framework and instantiate it
+const fastify = require("fastify")({
+  // Set this to true for detailed logging:
+  logger: false,
+});
+
+// ADD FAVORITES ARRAY VARIABLE FROM TODO HERE
+
+// Setup our static files
+fastify.register(require("@fastify/static"), {
+  root: path.join(__dirname, "."),
+  prefix: "/", // optional: default '/'
+});
+
+// View is a templating manager for fastify
+fastify.register(require("@fastify/view"), {
+  engine: {
+    handlebars: require("handlebars"),
+  },
+});
+
+/**
+ * Our home page route
+ *
+ * Returns src/pages/index.hbs with data built into it
+ */
+fastify.get("/", function (request, reply) {
+  // params is an object we'll pass to our handlebars template
+  let params = { name: "Abraham Daniel" };
+
+  // If someone clicked the option for a random color it'll be passed in the querystring
+
+  // The Handlebars code will be able to access the parameter values and build them into the page
+  return reply.view("index.hbs", params);
+});
+
+// Run the server and report out to the logs
+fastify.listen(
+  { port: process.env.PORT, host: "0.0.0.0" },
+  function (err, address) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    console.log(`Your app is listening on ${address}`);
+  }
+);
